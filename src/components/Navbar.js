cc40ef3253ceda17fa8c807421c39fb0f3600f03
alfonso <alfmcalle@gmail.com>
2019-08-21 12:46:04 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,34 +60,40 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
+              <Link className="navbar-item" to="/products">
+                Nuestros Casos de Éxito
+              </Link>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
+              {
+              // <Link className="navbar-item" to="/blog">
+              //   Blog
+              // </Link>
+            }
               <Link className="navbar-item" to="/contact">
-                Contact
+                Contacto
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
+              {
+                // <Link className="navbar-item" to="/contact/examples">
+                //   Form Examples
+                // </Link>
+              }
             </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
+            {
+            // <div className="navbar-end has-text-centered">
+            //   <a
+            //     className="navbar-item"
+            //     href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+            //     target="_blank"
+            //     rel="noopener noreferrer"
+            //   >
+            //     <span className="icon">
+            //       <img src={github} alt="Github" />
+            //     </span>
+            //   </a>
+            // </div>
+          }
           </div>
         </div>
       </nav>
