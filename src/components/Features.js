2752f34210e88f0b2a8150aec1acdcf5154e7900
alfonso <alfmcalle@gmail.com>
2019-09-03 20:56:41 +0200

    Update Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -24,9 +24,7 @@ const FeatureGrid = ({ gridItems }) => (
             <p>{item.text}</p>
           </div>
             {(item.url) && (
-              <a className="btn" href={item.url} target="_blank">
-                Saber más
-              </a>
+              <a className="btn" href="/contact" target="_blank">Saber más</a>
             )}
         </section>
       </div>
